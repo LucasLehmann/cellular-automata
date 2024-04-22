b49ed13e9b2794a242f0c3b8da77b31883ca3d8f
LucasLehmann <141682403+LucasLehmann@users.noreply.github.com>
2024-04-22 13:37:26 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 
 struct Game {
     board: Vec<Vec<Cell>>,
@@ -18,6 +18,12 @@ impl From<bool> for Cell {
     }
 }
 
+impl From<usize> for Cell {
+    fn from(value: usize) -> Self {
+        Cell { state: value }
+    }
+}
+
 struct Config {
     wrap_x: bool,
     wrap_y: bool,
@@ -32,33 +38,72 @@ impl Game {
         self.board[0].len()
     }
 
-    fn render_frame(&self) -> String {
+    fn iframe(&self) -> String {
         // print!("\x1B[2J\x1B[1;1H"); // Clear screen and move cursor to top
-        let separator = String::from("-").repeat(self.x_len());
+        let separator = String::from("-").repeat(self.y_len());
         let mut game_frame = String::with_capacity((self.x_len() + 3) * (self.y_len() + 2)); //"\x1B[2J\x1B[1;1H".to_string();
         game_frame.push_str("\x1B[2J\x1B[1;1H");
         game_frame.push_str(&format!("|{separator}|\n"));
         for line in self.board.iter() {
             let line = line
                 .into_iter()
-                .map(|x| if x.state == 1 { "#" } else { " " })
-                .fold(String::with_capacity(self.y_len()), |acc, x| acc + x);
+                .map(|x| if x.state != 0 { "#" } else { " " })
+                .fold(String::with_capacity(self.x_len()), |acc, x| acc + x);
             game_frame.push_str(&format!("|{line}|\n"));
         }
         game_frame.push_str(&format!("|{separator}|\n"));
         game_frame
     }
 
-    fn partial_render(&self) {
-        todo!("test");
+    fn partial_render(&self, delta: &Vec<(usize, usize)>) {
+        let mut buff = String::new();
+        for i in delta {
+            buff.push_str(&format!(
+                "\x1B[{};{}H{}",
+                i.0 + 2,
+                i.1 + 2,
+                if self.board[i.0][i.1].state != 0 {
+                    "#"
+                } else {
+                    " "
+                }
+            ));
+        }
+        buff.push_str(&format!("\x1B[{};1H", self.y_len()));
+        print!("{buff}");
     }
 
-    fn tick(&self) -> Vec<(usize, usize)> {
+    fn tick(&mut self) -> Vec<(usize, usize)> {
         // Any live cell with fewer than two live neighbors dies, as if by underpopulation.
         // Any live cell with two or three live neighbors lives on to the next generation.
         // Any live cell with more than three live neighbors dies, as if by overpopulation.
         // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-        todo!();
+        let mut frame = self.board.clone();
+        let mut delta = vec![];
+
+        for i in 0..self.x_len() {
+            for j in 0..self.y_len() {
+                let weight = self.weight(i, j);
+                let cell = &mut frame[i][j].state;
+                match weight {
+                    _ if weight < 2 && *cell == 1 => {
+                        *cell = 0;
+                        delta.push((i, j))
+                    }
+                    _ if weight == 3 && *cell == 0 => {
+                        *cell = 1;
+                        delta.push((i, j))
+                    }
+                    _ if weight > 3 && *cell == 1 => {
+                        *cell = 0;
+                        delta.push((i, j))
+                    }
+                    _ => {}
+                }
+            }
+        }
+        self.board = frame;
+        delta
     }
 
     fn weight(&self, a: usize, b: usize) -> usize {
@@ -101,7 +146,7 @@ impl Game {
                 if i == a && j == b {
                     continue;
                 }
-                if self.board[i][j].state == 1 {
+                if self.board[i][j].state != 0 {
                     count += 1;
                 }
             }
@@ -129,9 +174,21 @@ fn main() {
                     },
                 };
             } else if let Some((Width(w), Height(h))) = terminal_size() {
-                game.board = vec![vec![Cell { state: 0 }; w as usize - 2]; h as usize - 4];
+                game = Game {
+                    board: vec![vec![Cell { state: 0 }; w as usize - 2]; h as usize - 4],
+                    config: Config {
+                        wrap_x: true,
+                        wrap_y: true,
+                    },
+                };
             } else {
-                game.board = vec![vec![Cell { state: 0 }; 10]; 10];
+                game = Game {
+                    board: vec![vec![Cell { state: 0 }; 10]; 10],
+                    config: Config {
+                        wrap_x: true,
+                        wrap_y: true,
+                    },
+                }
             }
         }
 
@@ -145,7 +202,7 @@ fn main() {
         }
     }
 
-    println!("{}", game.render_frame());
+    println!("{}", game.iframe());
     let mut full_render: u8 = 0;
     let mut d1 = vec![];
     let mut d2 = vec![];
@@ -156,19 +213,11 @@ fn main() {
         // Left: \u001b[{n}D
         // print!("\x1B[2J\x1B[1;1H"); // Clear screen and move cursor to top
 
-        let delta = tick(&game);
-        for (a, b) in &delta {
-            game[*a][*b] = !game[*a][*b];
-            print!(
-                "\x1B[{};{}H{}",
-                *a + 2,
-                *b + 2,
-                if game[*a][*b] { "#" } else { " " }
-            );
-        }
+        let delta = game.tick();
+        game.partial_render(&delta);
 
         if full_render == 0 {
-            println!("{}", render_frame(&game));
+            print!("{}", game.iframe());
         }
         full_render = (full_render + 1) % u8::MAX;
 
@@ -177,7 +226,7 @@ fn main() {
         std::thread::sleep(std::time::Duration::from_millis(10));
 
         if delta == d1 || delta == d2 {
-            println!("repeating pattern");
+            print!("repeating pattern");
             break;
         } else {
             d1 = d2;
